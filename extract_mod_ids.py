af7f526761dea5a442e2dde7cc2a123d2bf72229
spacecheese <12695808+spacecheese@users.noreply.github.com>
2021-01-09 04:41:20 +0100

    Use all lower case in argument help (to match -h)

--- extract_mod_ids.py
+++ extract_mod_ids.py
@@ -1,10 +1,10 @@
 import preset_common as common
 import argparse
 
-parser = argparse.ArgumentParser(prog="extract_load_order.py", usage="%(prog)s [preset] [options]",
+parser = argparse.ArgumentParser(prog="extract_mod_ids.py", usage="%(prog)s [preset] [options]",
     description="Reads the mod ids to a .txt file.")
 parser.add_argument("preset")
-parser.add_argument("-o", "--output", help="The file to write mod ids to (defaults to mods.txt).")
+parser.add_argument("-o", "--output", help="file to write mod ids to (defaults to mods.txt)", default="mods.txt")
 args = parser.parse_args()
 
 preset = common.getPreset(args.preset)
