e16805c9d6a26f2686827b8770488acb174256ed
spacecheese <12695808+spacecheese@users.noreply.github.com>
2021-01-09 21:31:47 +0100

    Add escape option to extract_load_order

--- extract_load_order.py
+++ extract_load_order.py
@@ -3,16 +3,20 @@ import argparse
 
 parser = argparse.ArgumentParser(
     prog="extract_load_order.py",
-    usage="%(prog)s [preset]",
+    usage="%(prog)s [preset] [options]",
     description="Reads a presets mods out in the assigned load order formatted for use in the arma '-mods=' argument.",
 )
 parser.add_argument("preset")
+parser.add_argument("-e", "--escape", help="escapes spaces so that the load order can be used for linux servers", action="store_true")
 args = parser.parse_args()
 
 preset = common.getPreset(args.preset)
 
 loadString = ""
 for mod in preset.mods:
-    loadString += "@" + mod.name + ";"
+    if args.escape:
+        loadString += "@" + mod.name.replace(" ", "\\ ")
+    else
+        loadString += "@" + mod.name + ";"
 
 print(loadString)
