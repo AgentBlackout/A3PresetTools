c65d11d9e387b5f61a619fcf19ef59400493f07f
jw10g18 <jw10g18@soton.ac.uk>
2022-01-19 01:39:50 +0000

    Add mod sorting option

--- total_modset_size.py
+++ total_modset_size.py
@@ -6,10 +6,18 @@ import math
 
 parser = argparse.ArgumentParser(
     prog="total_modset_size.py",
-    usage="%(prog)s [modset]",
+    usage="%(prog)s [modset] [options]",
     description="Computes the combined size of all mods in a preset or workshop collection.",
 )
 parser.add_argument("modset")
+parser.add_argument(
+    "-s",
+    "--sort",
+    choices=["S", "s", "N", "n"],
+    help="sorts the modset in the specified order: S- size descending, s- size ascending, N- name descending, n- name ascending, No value- no sort",
+    default="S",
+    nargs="?"
+)
 args = parser.parse_args()
 
 
@@ -26,14 +34,21 @@ def format_bytes(num):
         return str(num) + " bytes"
 
 
-modset = common.ModSet.from_collection_preset(args.modset)
+def sort(mod):
+    if args.sort.lower() == "s":
+        return -1 if mod.size is None else mod.size
+    elif args.sort.lower() == "n":
+        return -1 if mod.name is None else mod.name.lower()
 
+
+modset = common.ModSet.from_collection_preset(args.modset)
 
 total_size = 0
 unknown_size = False
-modset.mods = sorted(
-    modset.mods, key=lambda mod: -1 if mod.size is None else mod.size, reverse=True
-)
+# Sort into ascending then reverse if descending is requested.
+if not args.sort is None:
+    modset.mods = sorted(modset.mods, key=sort, reverse=args.sort.isupper())
+
 for mod in modset.mods:
     if not mod.size is None:
         total_size += mod.size
